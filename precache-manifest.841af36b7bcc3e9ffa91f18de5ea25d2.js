33fc9cbbf78ce0adddcf864635b13039fea2c94f
Harsh4579 <jhavardhan4579@gmail.com>
2024-12-12 05:14:06 +0530

    Updates

---
+++ precache-manifest.841af36b7bcc3e9ffa91f18de5ea25d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba838f6dc7afe528fd035c419a75f0d7",
+    "url": "/Harsh4579/User_Authentication_Frontend/index.html"
+  },
+  {
+    "revision": "4262a4631e265e8642e4",
+    "url": "/Harsh4579/User_Authentication_Frontend/static/css/main.2c5d2651.chunk.css"
+  },
+  {
+    "revision": "ac4f3cffaae32a257cbe",
+    "url": "/Harsh4579/User_Authentication_Frontend/static/js/2.ee15d19e.chunk.js"
+  },
+  {
+    "revision": "4262a4631e265e8642e4",
+    "url": "/Harsh4579/User_Authentication_Frontend/static/js/main.a703da97.chunk.js"
+  },
+  {
+    "revision": "29466cad3adc4f17f027",
+    "url": "/Harsh4579/User_Authentication_Frontend/static/js/runtime-main.58f01df8.js"
+  }
+]);
